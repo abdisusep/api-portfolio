c33cc102adbdf2df8fb30b11f5b851bd69876521
Susep Dev <susep.dev@gmail.com>
2024-02-02 14:49:29 +0700

    02-02-2024

--- index.js
+++ index.js
@@ -3,17 +3,29 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
+const sequelize = require('./config/db.config');
 
 const mainRoutes = require('./routes');
 
 app.use('/api', mainRoutes);
 
 app.get('/', (req, res) => {
-  res.status(200).json({ 
-    name: process.env.BASE_URL,
-    node_version: process.version
-  });
+  try {
+    sequelize.authenticate();
+    res.status(200).json({ 
+      message: 'Connection has been established successfully.',
+      node_version: process.version
+    });
+  } catch (err) {
+    res.status(200).json({ 
+      message: err,
+      node_version: process.version
+    });
+  }
+  
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
