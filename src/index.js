f15416e6338f8189188fcb865ee788f3fc67f3fa
Susep Sp <susepsupriatna@gmail.com>
2024-04-12 18:04:43 +0700

    12-04-2024 | 18:04

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 4000;
 const db = require('./utils/db');
 const routes = require('./routes');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use('/api', routes);
 
